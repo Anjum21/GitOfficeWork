d89568ed84fae12380dad572da5cc1821e37822e
ANMONGA <ANMONGA@CGSCLRD47052768.in623.corpintra.net>
2017-01-04 10:34:37 +0530

    changes 3

--- GitOfficeWork/src/GitExample.java
+++ GitOfficeWork/src/GitExample.java
@@ -5,7 +5,7 @@ public class GitExample {
 		// TODO Auto-generated method stub
 
 		System.out.println("This is just to test in office");
-		System.out.println("These changes will be merged in branch");
+		System.out.println("These changes will be merged in branch 1");
 	}
 
 }
