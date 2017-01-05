deb83ae0f32f93b4868a7d9d617869bfa0c44834
anmonga <anmonga@CGSCLRD47052768.in623.corpintra.net>
2017-01-05 13:17:39 +0530

    The Initial practice demo

--- GitOfficeWork/src/GitExample.java
+++ GitOfficeWork/src/GitExample.java
@@ -4,10 +4,7 @@ public class GitExample {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("This is just to test in office");
-		System.out.println("These changes will");
-		
-		System.out.println(4-7);
+		System.out.println("This is just for practice");
 	}
 
 }
